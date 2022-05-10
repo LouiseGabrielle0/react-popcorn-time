4cb6395e040bbf78d1fdcafc83692731adcc15dc
Louise <louisegabriellecc@gmail.com>
2022-05-10 17:24:30 +0100

    Added Filter Button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,25 @@
 import moviesArr from "../data/movies.json";
 import './Main.css'
 import Movies from "./Movies"
+import {useState} from "react";
 
 function Main() {
 
+    const [movies, setMovies] = useState(moviesArr);
+
+    const filterTopRated = () => {
+        setMovies ((preValue) => preValue.filter(movies => movies.rating >= 8))
+        // setMovies(movies.filter(movies => movies.rating >= 8))
+    }
+
   return (
     <> 
-     {moviesArr.map((movie) => {
+
+    <div className="controls">
+        <button onClick={filterTopRated}>Display Top Rated Only</button>
+    </div>
+
+     {movies.map((movie) => {
       return (
           <Movies 
                 key={movie.key} 
